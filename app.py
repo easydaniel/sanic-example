eb98adab9f8bf728e8bebd216f9ffa1ab7333013
easydaniel <whitetiger50512@gmail.com>
2017-01-04 10:51:43 +0800

    add live reload and requirements

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from sanic import Sanic
+from aoiklivereload import LiveReloader
 import asyncio
 import uvloop
 
@@ -8,14 +9,29 @@ from blueprints import Blueprints
 
 from database import init_db
 
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-loop = asyncio.get_event_loop()
+def runserver():
+    try:
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-app = Sanic(__name__)
+        loop = asyncio.get_event_loop()
 
-app.blueprint(Blueprints.auth, url_prefix='/api/auth')
+        app = Sanic(__name__)
 
-loop.create_task(init_db())
+        app.blueprint(Blueprints.auth, url_prefix='/api/auth')
 
-app.go_fast(port=config.PORT, debug=config.DEBUG, workers=config.WORKERS, loop=loop)
+        loop.create_task(init_db())
+
+        if config.DEBUG:
+            reloader = LiveReloader()
+            reloader.start_watcher_thread()
+
+        app.go_fast(port=config.PORT, workers=config.WORKERS,
+                    debug=config.DEBUG, loop=loop)
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    exit(runserver())
