c23590428c296fc44485a9e564d02cda3e618337
easydaniel <whitetiger50512@gmail.com>
2017-01-04 11:47:08 +0800

    run server if only it is main module

--- app.py
+++ app.py
@@ -10,33 +10,27 @@ from blueprints import Blueprints
 from database import init_db
 
 
-def runserver():
-    try:
-        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-        loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop()
 
-        app = Sanic(__name__)
+app = Sanic(__name__)
 
-        app.blueprint(Blueprints.auth, url_prefix='/api/auth')
+app.blueprint(Blueprints.auth, url_prefix='/api/auth')
 
-        loop.create_task(init_db())
+loop.create_task(init_db())
 
-        if config.DEBUG:
-            reloader = LiveReloader()
-            reloader.start_watcher_thread()
+# TODO: Put somewhere for better code style
+if config.DEBUG:
+    reloader = LiveReloader()
+    reloader.start_watcher_thread()
 
-            @app.middleware('response')
-            async def request_log(request, response):
-                logging.info(
-                    f'{request.method} - {request.url} - {response.status}')
-
-        app.go_fast(port=config.PORT, workers=config.WORKERS,
-                    debug=config.DEBUG, loop=loop)
-
-    except KeyboardInterrupt:
-        pass
+    @app.middleware('response')
+    async def request_log(request, response):
+        logging.info(
+            f'{request.method} - {request.url} - {response.status}')
 
 
 if __name__ == '__main__':
-    exit(runserver())
+    app.go_fast(port=config.PORT, workers=config.WORKERS,
+                debug=config.DEBUG, loop=loop)
